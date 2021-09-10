1bb56b708048bc8949c8c323649c03976b4879ac
twenty <clarypong@gmail.com>
2021-09-10 17:57:39 +0700

    vuetify

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text1">5165454</div>
+          <v-btn
+  elevation="2"
+>test</v-btn>
+    </v-col>
+
+  </v-row>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.text1{
+  color:#666;
+}
+</style>
